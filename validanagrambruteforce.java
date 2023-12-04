e6b132349c37a81017dad57c79bc45e881bcb48b
Shreya Kakkar <90314788+ShreyaKakkarr@users.noreply.github.com>
2023-12-05 00:01:32 +0530

    Create validanagrambruteforce.java

---
+++ validanagrambruteforce.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sc = s.toCharArray();
+        char[] tc = t.toCharArray();
+        Arrays.sort(sc);
+        Arrays.sort(tc);
+        return new String(sc).equals(new String(tc));
+    }
+}
